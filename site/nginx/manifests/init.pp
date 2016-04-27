33c9830f2f3d7b33fb41a49d6e68a59e1387c40a
smilojkovic <milojkovic.slobodan@gmail.com>
2016-04-27 10:15:35 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,46 +1,39 @@
 class nginx {
+  File {
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+}
+
+$mod_root = 'puppet:///modules/nginx/'
+
   package { 'nginx':
     ensure => present,
 }
 
   file { '/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
     mode => '0775',
 }
 
   file { '/var/www/index.html':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/index.html',
+    source => "${mod_root}index.html",
 }
 
   file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/nginx.conf',
+    source =>  "${mod_root}nginx.conf",
     require => Package['nginx'],
     notify => Service['nginx'],
 }
 
   file { '/etc/nginx/conf.d':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
     mode => '0775',
 }
 
   file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/default.conf',
+    source => "${mod_root}default.conf",
     require => Package['nginx'],
     notify => Service['nginx'],
 }
