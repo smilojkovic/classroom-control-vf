6bf78be75816a94055c4cafeef12114f3faf09e7
smilojkovic <milojkovic.slobodan@gmail.com>
2016-04-26 19:31:07 -0700

    Create init.pp

---
+++ site/skeleton/examples/init.pp
@@ -0,0 +1,15 @@
+class skeleton {
+# create a directory      
+  file { '/etc/skel':
+    ensure => 'directory',
+}    
+# create file
+  file { '/etc/skel/.bashrc':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/skeleton/bashrc',
+}
+    
+}
