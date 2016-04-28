be5a4573ca7dbd34f3af10f4a3346fceca9f0020
smilojkovic <milojkovic.slobodan@gmail.com>
2016-04-27 19:29:49 -0700

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,13 +1,35 @@
 define users::managed_user (
-  $group = $title,
-) 
-{
+  $gid = $title,
+  $home = "home/${title}",
+  $shell = '/bin/bash',
+)  {
+
+# to prevent failures if run for multiple users
+if !defined(Group[$gid]) {
+  group { $gid:
+    ensure +> present
+  }
+}
+
   user { $title:
-    ensure => present,
+    ensure      => present,
+    gid         => $gid,
+    home        => $home,
+    shell       => $shell,
+    managehome  => true,
 }
-  file { "/home/${title}":
-    ensure => directory,
-    owner => $title,
-    group => $group,
+
+  file { $home:
+    ensure  => directory,
+    owner   => $title,
+    group   => $gid,
 }
+
+file { "${home}/.ssh":
+    ensure    => directory,
+    owner     => $title,
+    group     => $gid,
+    mode      => '0700'
+    require   => File[$home].
+  }  
 }
