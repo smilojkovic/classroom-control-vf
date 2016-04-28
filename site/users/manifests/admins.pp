4925371b4a831185cf316494bd71466e27495b69
smilojkovic <milojkovic.slobodan@gmail.com>
2016-04-27 20:03:36 -0700

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,13 +1,11 @@
 class users::admins {
 
-  users::managed_user { 'joe': }
-  users::managed_user { 'alice':
-  group => 'staff',
-}
-  users::managed_user { 'aaron':
-  group => 'staff',
+  users::managed_user { 'jose': }
+  users::managed_user { 'alice':}
+  users::managed_user { 'chen' : }
+    group => 'staff',
 }
   group { 'staff':
-  ensure => present,
+    ensure => present,
 }
 }
