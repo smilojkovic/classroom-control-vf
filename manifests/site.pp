47a27b25340fee044a85713794d4566d99d80ff3
smilojkovic <milojkovic.slobodan@gmail.com>
2016-04-28 09:46:40 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,10 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  
+  $message = hiera('message')
+    notify { $message: }
+}
 
   if $::virtual != 'physical' {
     $vmname = capitalize($::virtual)
